36925b904263c229137c5848533d494abe0f2b81
p1yu5h <piyush.satija@gmail.com>
2019-06-27 19:38:05 +0530

    Add check for location access.

--- app/src/main/java/com/example/android/airqualitymonitor/MainActivity.java
+++ app/src/main/java/com/example/android/airqualitymonitor/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.android.airqualitymonitor;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,6 +26,10 @@ import com.google.android.gms.location.LocationServices;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
     //Views
     private TextView aqiTextView, temperatureTextView, locationTextView, pressureTextView, humidityTextView, windTextView;
@@ -29,8 +40,13 @@ public class MainActivity extends AppCompatActivity {
     private Data data = new Data();
     private PollutantsAdapter pollutantsAdapter;
     private List<Pollutant> pollutantsList = new ArrayList<>();
+    private Call<ApiResponse> loadAQICall;
+    private RetrofitHelper retrofitHelper;
+    private ApiInterface apiInterface;
 
     FusedLocationProviderClient fusedLocationClient;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    Location lastLocation = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,49 +54,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         aqiViewModel = ViewModelProviders.of(this).get(AqiViewModel.class);
+        retrofitHelper = RetrofitHelper.getInstance();
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
-        }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, location -> {
-                    if (location != null) {
-                        String geo = "geo:" + location.getLatitude() + ";" + location.getLongitude();
-                        aqiViewModel.getStatus().observe(MainActivity.this, s -> {
-                            if (s != null) {
-                                if (s.equals("Fetching data...")) {
-                                    showDialog(s);
-                                } else dismissDialog();
-                            }
-                        });
-                        aqiViewModel.getGPSApiResponse(geo).observe(MainActivity.this, apiResponse -> {
-                            if (apiResponse != null) {
-                                data = apiResponse.getData();
-                                aqiTextView.setText(String.valueOf(data.getAqi()));
-                                setAqiScaleGroup();
-                                Iaqi iaqi = data.getIaqi();
-                                if (iaqi.getTemperature() != null)
-                                    temperatureTextView.setText(getString(R.string.temperature_unit_celsius, data.getIaqi().getTemperature().getV()));
-                                if (iaqi.getPressure() != null)
-                                    pressureTextView.setText(getString(R.string.pressure_unit, iaqi.getPressure().getV()));
-                                if (iaqi.getHumidity() != null)
-                                    humidityTextView.setText(getString(R.string.humidity_unit, iaqi.getHumidity().getV()));
-                                if (iaqi.getWind() != null)
-                                    windTextView.setText(getString(R.string.wind_unit, iaqi.getWind().getV()));
-                                locationTextView.setText(data.getCity().getName());
-                                setupRecyclerView();
-                                addPollutantsToList(data.getIaqi());
-                            }
-                        });
-                    }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                checkLocationPermission();
+            } else {
+                fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                    lastLocation = location;
+                    loadAQIData(lastLocation.getLatitude(), lastLocation.getLongitude());
                 });
+
+            }
+        }
+
         /*aqiViewModel.getStatus().observe(this, s -> {
             if (s != null) {
                 if (s.equals("Fetching data...")) {
@@ -139,7 +126,8 @@ public class MainActivity extends AppCompatActivity {
         TextView aqiScaleText;
         if (aqi >= 0 && aqi <= 50) aqiScaleText = findViewById(R.id.scaleGood);
         else if (aqi >= 51 && aqi <= 100) aqiScaleText = findViewById(R.id.scaleModerate);
-        else if (aqi >= 101 && aqi <= 150) aqiScaleText = findViewById(R.id.scaleUnhealthySensitive);
+        else if (aqi >= 101 && aqi <= 150)
+            aqiScaleText = findViewById(R.id.scaleUnhealthySensitive);
         else if (aqi >= 151 && aqi <= 200) aqiScaleText = findViewById(R.id.scaleUnhealthy);
         else if (aqi >= 201 && aqi <= 300) aqiScaleText = findViewById(R.id.scaleVeryUnhealthy);
         else if (aqi >= 301) aqiScaleText = findViewById(R.id.scaleVeryUnhealthy);
@@ -155,5 +143,153 @@ public class MainActivity extends AppCompatActivity {
 
     private void dismissDialog() {
         RetrofitHelper.getInstance().dismissProgressDialog();
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Allow Location Access")
+                        .setMessage("Allow Location Access")
+                        .setPositiveButton("Ok", (dialogInterface, i) -> {
+                            //Prompt the user once explanation has been shown
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    MY_PERMISSIONS_REQUEST_LOCATION);
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        fusedLocationClient.getLastLocation().addOnSuccessListener(location -> lastLocation = location);
+                        loadAQIData(lastLocation.getLatitude(), lastLocation.getLongitude());
+                    }
+
+                } else {
+                    finish();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getAqiData() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        String geo = "geo:" + location.getLatitude() + ";" + location.getLongitude();
+                        aqiViewModel.getStatus().observe(MainActivity.this, s -> {
+                            if (s != null) {
+                                if (s.equals("Fetching data...")) {
+                                    showDialog(s);
+                                } else dismissDialog();
+                            }
+                        });
+                        aqiViewModel.getGPSApiResponse(geo).observe(MainActivity.this, apiResponse -> {
+                            if (apiResponse != null) {
+                                Log.d("api", String.valueOf(apiResponse));
+                                data = apiResponse.getData();
+                                aqiTextView.setText(String.valueOf(data.getAqi()));
+                                setAqiScaleGroup();
+                                Iaqi iaqi = data.getIaqi();
+                                if (iaqi.getTemperature() != null)
+                                    temperatureTextView.setText(getString(R.string.temperature_unit_celsius, data.getIaqi().getTemperature().getV()));
+                                if (iaqi.getPressure() != null)
+                                    pressureTextView.setText(getString(R.string.pressure_unit, iaqi.getPressure().getV()));
+                                if (iaqi.getHumidity() != null)
+                                    humidityTextView.setText(getString(R.string.humidity_unit, iaqi.getHumidity().getV()));
+                                if (iaqi.getWind() != null)
+                                    windTextView.setText(getString(R.string.wind_unit, iaqi.getWind().getV()));
+                                locationTextView.setText(data.getCity().getName());
+                                setupRecyclerView();
+                                addPollutantsToList(data.getIaqi());
+                            }
+                        });
+                    }
+                });
+    }
+
+    private void loadAQIData(double latitude, double longitude) {
+        apiInterface = retrofitHelper.getApiInterface();
+        String geo = "geo:" + latitude + ";" + longitude;
+        loadAQICall = apiInterface.getLocationAQI(geo, "demo");
+        loadAQICall.enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<ApiResponse> call, @NonNull Response<ApiResponse> response) {
+                if (!response.isSuccessful()) {
+                    return;
+                }
+
+                if (response.body() == null) {
+                    return;
+                }
+                data = response.body().getData();
+                aqiTextView.setText(String.valueOf(data.getAqi()));
+                setAqiScaleGroup();
+                Iaqi iaqi = data.getIaqi();
+                if (iaqi.getTemperature() != null)
+                    temperatureTextView.setText(getString(R.string.temperature_unit_celsius, data.getIaqi().getTemperature().getV()));
+                if (iaqi.getPressure() != null)
+                    pressureTextView.setText(getString(R.string.pressure_unit, iaqi.getPressure().getV()));
+                if (iaqi.getHumidity() != null)
+                    humidityTextView.setText(getString(R.string.humidity_unit, iaqi.getHumidity().getV()));
+                if (iaqi.getWind() != null)
+                    windTextView.setText(getString(R.string.wind_unit, iaqi.getWind().getV()));
+                locationTextView.setText(data.getCity().getName());
+                setupRecyclerView();
+                addPollutantsToList(data.getIaqi());
+                Log.d("response", String.valueOf(response.body()));
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ApiResponse> call, Throwable t) {
+                Log.d("Error", "erorr");
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (lastLocation != null) loadAQIData(lastLocation.getLatitude(), lastLocation.getLongitude());
     }
 }

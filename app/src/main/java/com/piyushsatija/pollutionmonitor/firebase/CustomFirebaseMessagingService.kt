e7fa70ca537b70a12dc8323106ad2b3dcc758ee6
p1yu5h <piyush.satija@gmail.com>
2020-06-10 02:13:06 +0530

    Fix lint warnings

--- app/src/main/java/com/piyushsatija/pollutionmonitor/firebase/CustomFirebaseMessagingService.kt
+++ app/src/main/java/com/piyushsatija/pollutionmonitor/firebase/CustomFirebaseMessagingService.kt
@@ -4,11 +4,5 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class CustomFirebaseMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(s: String) {
-        super.onNewToken(s)
-    }
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
-    }
 }
--- app/src/main/java/com/piyushsatija/pollutionmonitor/model/Iaqi.kt
+++ app/src/main/java/com/piyushsatija/pollutionmonitor/model/Iaqi.kt
@@ -4,45 +4,45 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.piyushsatija.pollutionmonitor.model.properties.*
 
-public class Iaqi {
+class Iaqi {
     @SerializedName("co")
     @Expose
-    public var co: Co? = null
+    var co: Co? = null
 
     @SerializedName("no2")
     @Expose
-    public var no2: No2? = null
+    var no2: No2? = null
 
     @SerializedName("o3")
     @Expose
-    public var o3: O3? = null
+    var o3: O3? = null
 
     @SerializedName("p")
     @Expose
-    public var pressure: Pressure? = null
+    var pressure: Pressure? = null
 
     @SerializedName("pm10")
     @Expose
-    public var pm10: Pm10? = null
+    var pm10: Pm10? = null
 
     @SerializedName("pm25")
     @Expose
-    public var pm2_5: Pm2_5? = null
+    var pm2_5: Pm2_5? = null
 
     @SerializedName("so2")
     @Expose
-    public var so2: So2? = null
+    var so2: So2? = null
 
     @SerializedName("t")
     @Expose
-    public var temperature: Temperature? = null
+    var temperature: Temperature? = null
 
     @SerializedName("h")
     @Expose
-    public var humidity: Humidity? = null
+    var humidity: Humidity? = null
 
     @SerializedName("w")
     @Expose
-    public var wind: Wind? = null
+    var wind: Wind? = null
 
 }
